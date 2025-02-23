8d2f2330f3787c4a6cd1d38d456c4fd52b871b81
isaac <isaac.levi@outlook.com>
2025-02-23 11:04:06 -0300

    Update gameplay.py

--- gameplay.py
+++ gameplay.py
@@ -11,8 +11,8 @@ env = make_atari_env('ALE/Frogger-v5', n_envs=1, seed=0)
 env = VecFrameStack(env, n_stack=4) #Adiciona noção de tempo
 
 #Carregue o qualquer modelo treinado na pasta models, escolha a versão
-models_dir = "models/CNNzz"
-model_path = f"{models_dir}/300000.zip"
+models_dir = "models/CNN" #Exemplo
+model_path = f"{models_dir}/480000.zip" #Exemplo
 model = DQN.load(model_path, env = env)
 
 # Avaliaçao
@@ -20,9 +20,9 @@ episode_rewards = evaluate_policy(model, env, n_eval_episodes=10, render=True, r
 media = np.mean(episode_rewards[0])
 desvio_padrao = np.std(episode_rewards[0])
 
-print(f"Mean reward: {media}")
-print(f"Standard deviation of reward: {desvio_padrao}")
-print(f"Episode rewards: {episode_rewards[0]}")
+print(f"Média das recompensas: {media}")
+print(f"Desvio Padrão da recompensa: {desvio_padrao}")
+print(f"Recompensas por episódio: {episode_rewards[0]}")
 
 #Render para acompanhar o modelo jogando(so encerra no terminal)
 obs = env.reset()
