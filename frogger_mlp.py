d213818d23e7b7f2f192dc24c86fe0c39ec4f863
isaac <isaac.levi@outlook.com>
2025-02-23 22:25:33 -0300

    Update frogger_mlp.py

--- frogger_mlp.py
+++ frogger_mlp.py
@@ -59,7 +59,7 @@ model = DQN("MlpPolicy",
              learning_starts=20000,
              buffer_size=TAM_BUFFER)
 
-#Treinamento do modelo, vai salvar uma um modelo a cada 10k timesteps
+#Treinamento do modelo, vai salvar uma um modelo a cada 50K timesteps
 #Visualize o desempenho com o tensorboard: python -m tensorboard.main --logdir=logs
 TIMESTEPS = 50000
 for i in range (1,11):
