47acc6e9693a439bf29d7246e06674ce0141c613
isaac <isaac.levi@outlook.com>
2025-02-24 20:53:35 -0300

    Update gameplay.py

--- gameplay.py
+++ gameplay.py
@@ -5,24 +5,26 @@ from stable_baselines3.common.env_util import make_atari_env
 from stable_baselines3.common.vec_env import VecFrameStack
 from stable_baselines3.common.evaluation import evaluate_policy
 import numpy as np
+import time
 
 gym.register_envs(ale_py)
 env = make_atari_env('ALE/Frogger-v5', n_envs=1, seed=0)
 env = VecFrameStack(env, n_stack=4) #Adiciona noção de tempo
 
 #Carregue o qualquer modelo treinado na pasta models, escolha a versão
-models_dir = "models/CNN" #Exemplo
-model_path = f"{models_dir}/480000.zip" #Exemplo
+models_dir = "models/CNN1m"
+model_path = f"{models_dir}/1700000.zip"
 model = DQN.load(model_path, env = env)
 
 # Avaliaçao
-episode_rewards = evaluate_policy(model, env, n_eval_episodes=10, render=True, return_episode_rewards=True)
-media = np.mean(episode_rewards[0])
-desvio_padrao = np.std(episode_rewards[0])
+episode_rewards, episode_lengths = evaluate_policy(model, env, n_eval_episodes=10, render=False, return_episode_rewards=True)
+media = np.mean(episode_rewards)
+desvio_padrao = np.std(episode_rewards)
 
-print(f"Média das recompensas: {media}")
-print(f"Desvio Padrão da recompensa: {desvio_padrao}")
-print(f"Recompensas por episódio: {episode_rewards[0]}")
+print(f"Mean reward: {media}")
+print(f"Standard deviation of reward: {desvio_padrao}")
+print(f"Episode rewards: {episode_rewards}")
+print(f"Episode lengths: {episode_lengths}")
 
 #Render para acompanhar o modelo jogando(so encerra no terminal)
 obs = env.reset()
@@ -30,4 +32,5 @@ while True:
     action, _ = model.predict(obs)
     obs, rewards, dones, info = env.step(action)
     env.render("human")
+    time.sleep(0.05)
     
